bb4cd1cedd99a5c008793d7c3595733301d18655
bluedeviltms-beep <bluedeviltms@gmail.com>
2026-08-30 16:17:14 +0900

    Deploy Render backend configuration

--- git-deploy.ps1
+++ git-deploy.ps1
@@ -1,17 +1,18 @@
 $git = 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Git\cmd\git.exe'
 
-cmdkey /delete:LegacyGeneric:target=git:https://github.com 2>$null
-cmdkey /delete:git:https://github.com 2>$null
-
 & $git config user.email "bluedeviltms@gmail.com"
 & $git config user.name "bluedeviltms-beep"
 
 if (!(Test-Path '.git')) {
     & $git init
-    & $git remote add origin https://github.com/bluedeviltms-beep/maehwa-downloader-web.git
+}
+
+& $git remote set-url origin https://bluedeviltms-beep@github.com/bluedeviltms-beep/maehwa-downloader-web.git 2>$null
+if ($LASTEXITCODE -ne 0) {
+    & $git remote add origin https://bluedeviltms-beep@github.com/bluedeviltms-beep/maehwa-downloader-web.git
 }
 
 & $git branch -M main
 & $git add .
-& $git commit -m "Deploy Render backend configuration"
+& $git commit -m "Deploy Render backend configuration" 2>$null
 & $git push -u origin main --force
